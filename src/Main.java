33d76bea85785360b4b88585129f16655fc69bb0
sifatredwan <wahid016@d.umn.edu>
2025-03-05 16:51:20 -0600

    Added datawriter

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        String filename = "outputfile.txt";
 
         System.out.println("Hello world");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
+            writer.write("You can output a string quite easilty");
+            writer.newLine();
+            writer.write("Adding another line for output");
+
+            System.out.println("File written successfully");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 }
